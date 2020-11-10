3c58a1d4adba4788fbaa1a38d883ab9a262bea3d
EQ1 <bluetwintail@naver.com>
2020-11-10 14:32:35 +0900

    Initial Commit

---
+++ Unsplash/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Unsplash
+//
+//  Created by EQ1's Mac on 2020/11/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Unsplash/UnsplashApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsplashApp.swift
+//  Unsplash
+//
+//  Created by EQ1's Mac on 2020/11/10.
+//
+
+import SwiftUI
+
+@main
+struct UnsplashApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
