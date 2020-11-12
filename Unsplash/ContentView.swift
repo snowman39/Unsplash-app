c78e961bec4601186c8b0b46f81dba7172e24f1c
EQwon <bluetwintail0906@gmail.com>
2020-11-12 11:16:38 +0900

    Random List

--- Unsplash/ContentView.swift
+++ Unsplash/ContentView.swift
@@ -6,11 +6,28 @@
 //
 
 import SwiftUI
+import SDWebImageSwiftUI
 
 struct ContentView: View {
+    
+    @ObservedObject var randomImages = UnsplashData()
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVStack {
+                ForEach(randomImages.photoArray, id: \.id) { photo in
+                    WebImage(url: URL(string: photo.urls["thumb"]!))
+                        .resizable()
+                        .aspectRatio(contentMode: .fill)
+                        .frame(width: UIScreen.main.bounds.width - 50, height: 200, alignment: .center)
+                        .cornerRadius(15)
+
+                    if photo.alt_description != nil {
+                        Text(photo.alt_description!).font(.footnote)
+                    }
+                }
+            }.padding(20)
+        }.navigationTitle("Random List")
     }
 }
 
