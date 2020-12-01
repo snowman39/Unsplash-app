bfc59ed5b03b9589f9ecd904d02835cd164f4c03
EQwon <bluetwintail0906@gmail.com>
2020-12-01 14:43:16 +0900

    Merge peng-u to gyuwon

---
+++ UnsplashPractice/ContentView.swift
@@ -0,0 +1,47 @@
+//
+//  ContentView.swift
+//  UnsplashPractice
+//
+//  Created by 최유림 on 2020/11/26.
+//
+
+import SwiftUI
+import SDWebImageSwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Home()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+
+struct Indicator: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        
+        let view = UIActivityIndicatorView(style: .large)
+        view.startAnimating()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        
+    }
+}
+
+struct Indicator_small: UIViewRepresentable {
+    func makeUIView(context: Context) -> UIActivityIndicatorView {
+        
+        let view = UIActivityIndicatorView(style: .medium)
+        view.startAnimating()
+        return view
+    }
+    
+    func updateUIView(_ uiView: UIActivityIndicatorView, context: Context) {
+        
+    }
+}
